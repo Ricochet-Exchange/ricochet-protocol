094c61c9992e9842bc04e3e24db1decc8ba9dcb2
Michael Ghen <mike@mikeghen.com>
2021-11-15 10:31:09 -0500

    Modify data structures

--- contracts/REXMarketBase.sol
+++ contracts/REXMarketBase.sol
@@ -1,5 +1,34 @@
 contract REXMarketBase {
 
+  struct SubsidyPool {
+      ISuperToken token;
+      uint256 emissionRate;      // Rate to emit tokens if there's a balance, used for subsidies
+  }
+
+  struct LiquidityPool {
+    ISuperToken inputToken;
+    ISuperToken outputToken;
+    uint128 feeRate;
+  }
+
+  struct Market {
+    LiquidityPool poolA;
+    LiquidityPool poolB;
+    uint256 lastDistributionAt;                   // The last time a distribution was made
+    uint256 rateTolerance;                        // The percentage to deviate from the oracle scaled to 1e6
+    address owner;                                // The owner of the market (reciever of fees)
+    mapping(address => uint256) oracleRequestIds; // Maps tokens to their oracle request ID
+    mapping(uint32 => SubsidyPool) subsidyPools;  // Maps IDA indexes to their distributed Supertokens
+    uint8 numSubsidyPools;                        // Indexes outputPools and outputPoolFees
+  }
+
+  ISuperfluid host;                     // Superfluid host contract
+  IConstantFlowAgreementV1 cfa;         // The stored constant flow agreement class address
+  IInstantDistributionAgreementV1 ida;  // The stored instant dist. agreement class address
+  IUniswapV2Router02 router;            // Address of uniswap compatible router
+  ITellor oracle;                       // Address of deployed simple oracle for input//output token
+  Market market;
+
 // Custom functionality that needs to be overrided by contract extending the base
 
   // Converts input token to output token
@@ -7,10 +36,6 @@ contract REXMarketBase {
 
   // Harvests rewards if any
   function harvest() public virtual;
-
-  // Send fees accured to the owner
-  function claim() public virtual;
-
 
 // Standardized functionality for all REX Markets
 
@@ -46,6 +71,5 @@ contract REXMarketBase {
   function _deleteShareholder(address shareholder) { }
 
   function _getShareholderInfo(bytes calldata _agreementData) { }
-
 
 }
