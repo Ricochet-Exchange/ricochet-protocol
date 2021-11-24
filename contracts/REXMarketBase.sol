7c0e2801882eb54be1c98b45cdcddf94bcb88ece
Michael Ghen <mike@mikeghen.com>
2021-11-24 10:45:44 -0500

    REX market base work, extending base work in progress

--- contracts/REXMarketBase.sol
+++ contracts/REXMarketBase.sol
@@ -1,7 +1,33 @@
 contract REXMarketBase {
 
+  // REX Market Base Contract
+  //
+  // Responsibilities:
+  // - Reusable superfluid functionality
+  // - Oracle management functionality
+  // - IDA pool share management functionality
+  //
+  // Deployment Sequence:
+  // - Construct the contract with the Superfluid and owner info
+  // - Initialize the input token and props (initializeMarket)
+  // - Add first output pool which is the main output token sent (addOutputPool)
+  // - Add second output pool which is the subsidy token send (addOutputPool)
+  //
+  // Extending REX Markets:
+  // - Contract should be extended and the extending contract should override:
+  //   - distribute() - must take accumulated input tokens, convert to
+  //                    output tokens and distribute to output pool
+  //   - harvest() - (optional) must harvest yield, aggregate it such so that
+  //                 the distrute method can distribute it
+
+  struct OracleInfo {
+    uint256 requestId;
+    uint256 usdPrice;
+    uint256 lastUpdatedAt;
+  }
+
   struct OutputPool {
-      ISuperToken token;
+      Token token;
       uint128 feeRate;           // Fee taken by the DAO on each output distribution
       uint256 emissionRate;      // Rate to emit tokens if there's a balance, used for subsidies
   }
@@ -11,7 +37,7 @@ contract REXMarketBase {
     uint256 lastDistributionAt;                   // The last time a distribution was made
     uint256 rateTolerance;                        // The percentage to deviate from the oracle scaled to 1e6
     address owner;                                // The owner of the market (reciever of fees)
-    mapping(address => uint256) oracleRequestIds; // Maps tokens to their oracle request ID
+    mapping(address => OracleInfo) oracles;           // Maps tokens to their oracle info
     mapping(uint32 => OutputPool) outputPools;   // Maps IDA indexes to their distributed Supertokens
     uint8 numOutputPools;                        // Indexes outputPools and outputPoolFees
   }
@@ -20,13 +46,14 @@ contract REXMarketBase {
   IConstantFlowAgreementV1 cfa;         // The stored constant flow agreement class address
   IInstantDistributionAgreementV1 ida;  // The stored instant dist. agreement class address
   IUniswapV2Router02 router;            // Address of uniswap compatible router
-  ITellor oracle;                       // Address of deployed simple oracle for input//output token
+  ITellor tellor;                       // Address of deployed simple oracle for input//output token
   Market market;
 
-  constructor(ISuperfluid _host, IConstantFlowAgreementV1 _cfa, IInstantDistributionAgreementV1 _ida) public {
+  constructor(address _owner, ISuperfluid _host, IConstantFlowAgreementV1 _cfa, IInstantDistributionAgreementV1 _ida) public {
     host = _host;
     cfa = _cfa;
     ida = _ida;
+    owner = _owner;
   }
 
 // Market initialization methods
@@ -34,39 +61,74 @@ contract REXMarketBase {
   function initializeMarket(
     ISuperToken _inputToken,
     uint256 _rateTolerance,
-    address _owner) public onlyOwner {
+    ITellor _tellor,
+    uint256 _inputTokenRequestId) public onlyOwner {
 
+    require(market.inputToken == address(0), "!reinitable");
+    require(oracle == address(0), "!reinitable");
     market.inputToken = _inputToken;
     market.rateTolerance = _rateTolerance;
-    market.owner = _owner;
+    tellor = _tellor;
+    Oracle newOracle = new OracleInfo(_inputTokenRequestId, 0, 0);
+    // TODO: Check oracle and set init price, initialy set to 0s
+    market.oracles[market.inputToken] = newOracle;
   }
 
-  function initializeOracle(ITellor _oracle, uint256 _inputTokenRequestId) public onlyOwner {
-    require(market.inputToken != address(0), "!inputTokenSet");
-    oracle = _oracle;
-    market.oracleRequestIds[market.inputToken] = _inputTokenRequestId;
-  }
-
-  function addOutputPool(ISuperToken _token, uint128 _feeRate, uint256 _emissionRate, uint256 _requestId) public onlyOwner {
+  function addOutputPool(
+    ISuperToken _token,
+    uint128 _feeRate,
+    uint256 _emissionRate,
+    uint256 _requestId) public onlyOwner {
     // TODO: There's probably a maxiumum number of pools before the distribute method
-    //       will run out of gas, can't have like 20 pools and distribute to all
+    //       will run out of gas, limit should be the same for all markets
     require(_requestId != 0, "!validReqId");
+    require(market.oracleRequestIds[_token] == 0, "!unique");
     OutputPool newPool = new OutputPool(_token, _feeRate, _emissionRate);
     market.outputPools[market.numOutputPools] = newPool;
     _createIndex(market.numOutputPools, _token);
     market.numOutputPools++;
-    market.oracleRequestIds[_token] = _requestId;
+    Oracle newOracle = new OracleInfo(_requestId, 0, 0);
+    // TODO: Check oracle and set init price, initialy set to 0s
+    market.oracles[market.inputToken] = newOracle;
   }
 
 // Custom functionality that needs to be overrided by contract extending the base
 
   // Converts input token to output token
-  function distribute() public virtual {  }
+  function distribute(bytes memory _ctx) public virtual returns(bytes memory newCtx) {  }
 
   // Harvests rewards if any
-  function harvest() public virtual {  }
+  function harvest(bytes memory _ctx) public virtual returns (bytes memory newCtx) {  }
 
 // Standardized functionality for all REX Markets
+
+  // Oracle Functions
+
+  function updateTokenPrice(address _token) public {
+    (bool ifRetrieve,
+    uint256 value,
+    uint256 timestampRetrieved) = getCurrentValue(market.oracles[_token].requestId);
+    require(_didGet, "!getCurrentValue");
+    require(_timestamp >= block.timestamp - 3600, "!currentValue");
+    market.oracles[_token].usdPrice = _value;
+    market.oracles[_token].lastUpdatedAt = _timestamp;
+  }
+
+  function getCurrentValue(uint256 _requestId)
+    public view returns (
+        bool ifRetrieve,
+        uint256 value,
+        uint256 timestampRetrieved
+    )
+  {
+      uint256 _count = self.oracle.getNewValueCountbyRequestId(_requestId);
+      uint256 _time = self.oracle.getTimestampbyRequestIDandIndex(_requestId, _count - 1);
+      uint256 _value = self.oracle.retrieveData(_requestId, _time);
+      if (_value > 0) return (true, _value, _time);
+      return (false, 0, _time);
+  }
+
+  // Superfluid Functions
 
   function afterAgreementCreated(
     ISuperToken _superToken,
@@ -81,12 +143,14 @@ contract REXMarketBase {
     onlyExpected(_superToken, _agreementClass)
     returns (bytes memory newCtx)
   {
-      (address shareholder,
-       int96 flowRate) = _getShareholderInfo(_agreementData);
+    newCtx = ctx;
 
-      harvest();
-      distribute();
-      _updateShareholder(shareholder, flowRate);
+    (address shareholder,
+     int96 flowRate) = _getShareholderInfo(_agreementData);
+
+    newCtx = harvest(newCtx);
+    newCtx = distribute(newCtx);
+    newCtx = _updateShareholder(newCtx, shareholder, flowRate);
   }
 
   function afterAgreementUpdated(
@@ -102,12 +166,14 @@ contract REXMarketBase {
     onlyExpected(_superToken, _agreementClass)
     returns (bytes memory newCtx)
   {
-      (address shareholder,
-       int96 flowRate) = _getShareholderInfo(_agreementData);
+    newCtx = ctx;
 
-      harvest();
-      distribute();
-      _updateShareholder(shareholder, flowRate);
+    (address shareholder,
+     int96 flowRate) = _getShareholderInfo(_agreementData);
+
+    newCtx = harvest(newCtx);
+    newCtx = distribute(newCtx);
+    newCtx = _updateShareholder(newCtx, shareholder, flowRate);
   }
 
   function afterAgreementTerminated(
@@ -122,22 +188,19 @@ contract REXMarketBase {
     onlyHost
     returns (bytes memory newCtx)
   {
-      (address shareholder, ) = _getShareholderInfo(_agreementData);
-
-      _updateShareholder(shareholder, 0);
+    newCtx = ctx;
+    (address shareholder, ) = _getShareholderInfo(_agreementData);
+    newCtx = _updateShareholder(newCtx, shareholder, 0);
   }
-
-  function _createNewShareholder(address shareholder, int96 shareholderFlowRate) { }
 
   function _updateShareholder(bytes memory ctx, address shareholder, int96 shareholderFlowRate) internal returns (bytes memory newCtx) {
     // TODO: We need to make sure this for-loop won't run out of gas, do this we can set a limit on numOutputPools
     // We need to go through all the output tokens and update their IDA shares
     for (uint256 index = 0; index < market.numOutputPools; index++) {
       newCtx = _updateSubscriptionWithContext(newCtx, index, shareholder, uint128(uint(int(shareholderFlowRate))), market.outputPools[index].token);
+      // TODO: Update the fee taken by the DAO
     }
   }
-
-  function _deleteShareholder(address shareholder) { }
 
   function _getShareholderInfo(bytes calldata _agreementData) internal view returns(address shareholder, int96 flowRate) {
     (shareholder, ) = abi.decode(_agreementData, (address, address));
---
+++ contracts/REXOneWayMarket.sol
@@ -0,0 +1,58 @@
+contract REXOneWayMarket is REXMarketBase {
+
+  constructor(
+    address _token1,
+    uint256 _poolId,
+    ISuperfluid _host,
+    IConstantFlowAgreementV1 _cfa,
+    IInstantDistributionAgreementV1 _ida
+  ) public REXMarketBase(_host, _cfa, _ida) {
+
+    // TODO
+
+  }
+
+  function distribute(bytes memory ctx) external returns (bytes memory newCtx) {
+    newCtx = ctx;
+
+    // TODO: Move this to another function to reduce gas spend on distribute
+    // Get the exchange rate as inputToken per outputToken
+    bool _didGet;
+    uint _timestamp;
+    uint _value;
+   (_didGet, _value, _timestamp) = _getCurrentValue(market.oracleRequestIds[market.inputToken]);
+   require(_didGet, "!getCurrentValue");
+   require(_timestamp >= block.timestamp - 3600, "!currentValue");
+
+   _swap(self, ISuperToken(market.inputToken).balanceOf(address(this)), _value, block.timestamp + 3600);
+   // market.outputPools[0] MUST be the output token of the swap
+   uint256 outputBalance = market.outputPools[0].token.balanceOf(address(this));
+   (uint256 actualAmount,) = self.ida.calculateDistribution(
+        market.outputPools[0].token,
+        address(this),
+        0,
+        outputBalance);
+    // Return if there's not anything to actually distribute
+    if (actualAmount == 0) { return newCtx; }
+
+    // Calculate the fee for making the distribution
+    uint256 feeCollected = actualAmount * market.outputPools[0].feeRate / 1e6;
+    uint256 distAmount = actualAmount - feeCollected;
+
+    // Make the distribution
+    newCtx = _idaDistribute(self, self.outputIndexId, uint128(distAmount), self.outputToken, newCtx);
+    emit Distribution(distAmount, feeCollected, address(self.outputToken));
+
+    outputBalance = market.outputPools[1].token.balanceOf(addres(this));
+    if (market.numOutputPools > 1 &&  outputBalance > 0) {
+      // market.outputPools[1] MUST be the subsidy token
+      distAmount = (block.timestamp - self.lastDistributionAt) * self.subsidyRate;
+      if (distAmount < outputBalance) {
+        newCtx = _idaDistribute(self, self.subsidyIndexId, uint128(subsidyAmount), self.subsidyToken, newCtx);
+        emit Distribution(subsidyAmount, 0, address(self.subsidyToken));
+      }
+    }
+
+  }
+
+}
