f0438e394570df1834b412ecab5aa48249bbf839
josealonso <jose.alonso.programmer@gmail.com>
2022-01-26 23:51:09 +0100

    Created the config file for for the solidity-coverage plugin

---
+++ .solcover.js
@@ -0,0 +1,5 @@
+module.exports = {
+  skipFiles: ['IRicochetToken.sol', 'ITellor.sol', 'matic/IWMATIC.sol', 'mockTokens/CollateralToken.sol',
+    'mockTokens/DebtToken.sol', 'mockTokens/ISuperToken.sol', 'superfluid/IMATICx.sol', 'sushiswap/IMiniChefV2.sol',
+    'tellor/ITellor.sol', 'tellor/MockTellor.sol', 'tellor/TellorPlayground.sol', 'tellor/UsingTellor.sol']
+};
