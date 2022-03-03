5e9ab6e064d7a598b3894c36746fc1717474f65c
Tim Kernell <tpkernell@gmail.com>
2022-03-03 14:11:13 -0500

    update oracle to tellorFlex and mesosphere medianizer

--- contracts/tellor/ITellor.sol
+++ contracts/tellor/ITellor.sol
@@ -542,4 +542,20 @@ interface ITellor {
      * @return uint total supply
      */
     function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Gets median of oracle values within a given time interval
+     * @param _queryId is the id of the desired data
+     * @param _timestamp is the highest timestamp in the time interval
+     * @param _timeLimit is the length (seconds) of the time interval
+     * @param _maxValueCount is the max number of values used to calculate a median
+     * @return uint256 the median value
+     * @return uint256 the quantity of values used to determine the median
+     */
+    function getMedian(
+        bytes32 _queryId,
+        uint256 _timestamp,
+        uint256 _timeLimit,
+        uint256 _maxValueCount
+    ) public view returns (uint256, uint256);
 }
