13d9b4ab3a71f1eab451b41d51bf98bceb1e03cb
Mike Ghen <mike@mikeghen.com>
2022-05-23 12:14:08 -0400

    Bring in REX Launchpad to this repo (#112)

---
+++ scripts/deploy-launchpad.js
@@ -0,0 +1,65 @@
+async function main() {
+
+  const [deployer] = await ethers.getSigners();
+
+  // Polygon Mainnet
+  const HOST_ADDRESS = "0x3E14dC1b13c488a8d5D310918780c983bD5982E7";
+  const CFA_ADDRESS = "0x6EeE6060f715257b970700bc2656De21dEdF074C";
+  const IDA_ADDRESS = "0xB0aABBA4B2783A72C52956CDEF62d438ecA2d7a1";
+  const RIC_TREASURY_ADDRESS = "0x9C6B5FdC145912dfe6eE13A667aF3C5Eb07CbB89";
+  const OUTPUT_RATE = "32000000000000000"; // ~1M RIC/year
+  const FEE_RATE = "100000";  // 1
+  const INPUT_TOKEN_ADDRESS = "0xCAa7349CEA390F89641fe306D93591f87595dc1F";
+  const OUTPUT_TOKEN_ADDRESS = "0x263026E7e53DBFDce5ae55Ade22493f828922965"; //RIC
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  console.log("Deploying RicochetLaunchpadHelper")
+  const RicochetLaunchpadHelper = await ethers.getContractFactory("RicochetLaunchpadHelper");
+  let ricochetLaunchpadHelpder = await RicochetLaunchpadHelper.deploy();
+  console.log("Deployed RicochetLaunchpadHelper ")
+
+  const RicochetLaunchpad = await ethers.getContractFactory("RicochetLaunchpad", {
+    libraries: {
+      RicochetLaunchpadHelper: ricochetLaunchpadHelpder.address,
+    },
+  });
+  console.log("Deploying RicochetLaunchpad with params")
+  console.log("\tHOST_ADDRESS", HOST_ADDRESS)
+  console.log("\tCFA_ADDRESS", CFA_ADDRESS)
+  console.log("\tIDA_ADDRESS", IDA_ADDRESS)
+  console.log("\tINPUT_TOKEN", INPUT_TOKEN_ADDRESS)
+  console.log("\tOUTPUT_TOKEN", OUTPUT_TOKEN_ADDRESS)
+  console.log("\tSF_REG_KEY", process.env.SF_REG_KEY)
+
+
+
+  const ricochetLaunchpad = await RicochetLaunchpad.deploy( HOST_ADDRESS,
+                                                      CFA_ADDRESS,
+                                                      IDA_ADDRESS,
+                                                      process.env.SF_REG_KEY );
+  console.log("Deployed app, initializing...")
+  console.log(INPUT_TOKEN_ADDRESS,
+             OUTPUT_TOKEN_ADDRESS,
+             deployer.address,
+             RIC_TREASURY_ADDRESS,
+             OUTPUT_RATE,
+             FEE_RATE)
+  await ricochetLaunchpad.initialize(INPUT_TOKEN_ADDRESS,
+                       OUTPUT_TOKEN_ADDRESS,
+                       deployer.address,
+                       RIC_TREASURY_ADDRESS,
+                       OUTPUT_RATE,
+                       FEE_RATE);
+  await ricochetLaunchpad.deployed();
+  console.log("Deployed RicochetLaunchpadHelper at address:", ricochetLaunchpadHelpder.address);
+  console.log("Deployed RicochetLaunchpad at address:", ricochetLaunchpad.address);
+}
+
+main()
+.then(() => process.exit(0))
+.catch(error => {
+    console.error(error);
+    process.exit(1);
+});
