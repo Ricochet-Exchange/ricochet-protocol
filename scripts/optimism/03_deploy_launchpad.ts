014a907250b2908acaf7e0f3df416e465ea9983d
Michael Ghen <mike@mikeghen.com>
2023-03-29 11:44:22 -0400

    uncomment code in op launcpad deploy script

--- scripts/optimism/03_deploy_launchpad.ts
+++ scripts/optimism/03_deploy_launchpad.ts
@@ -17,46 +17,46 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // Deploy the RicochetLaunchpadHelper
-  const RicochetLaunchpadHelper = await ethers.getContractFactory("RicochetLaunchpadHelper");
-  let ricochetLaunchpadHelpder = await RicochetLaunchpadHelper.deploy();
-  await ricochetLaunchpadHelpder.deployed();
-  console.log("Deployed RicochetLaunchpadHelper:", ricochetLaunchpadHelpder.address)
+  // // Deploy the RicochetLaunchpadHelper
+  // const RicochetLaunchpadHelper = await ethers.getContractFactory("RicochetLaunchpadHelper");
+  // let ricochetLaunchpadHelpder = await RicochetLaunchpadHelper.deploy();
+  // await ricochetLaunchpadHelpder.deployed();
+  // console.log("Deployed RicochetLaunchpadHelper:", ricochetLaunchpadHelpder.address)
 
-  // Deploy the RicochetLaunchpad
-  const RicochetLaunchpad = await ethers.getContractFactory("RicochetLaunchpad", {
-    libraries: {
-      RicochetLaunchpadHelper: ricochetLaunchpadHelpder.address,
-    },
-  });
-  const ricochetLaunchpad = await RicochetLaunchpad.deploy(config.HOST_SUPERFLUID_ADDRESS,
-    config.CFA_SUPERFLUID_ADDRESS,
-    config.IDA_SUPERFLUID_ADDRESS,
-    config.SF_REG_KEY,
-    config.REX_REFERRAL_ADDRESS);
-  await ricochetLaunchpad.deployed();
-  console.log("Deployed RicochetLaunchpad:", ricochetLaunchpad.address)
+  // // Deploy the RicochetLaunchpad
+  // const RicochetLaunchpad = await ethers.getContractFactory("RicochetLaunchpad", {
+  //   libraries: {
+  //     RicochetLaunchpadHelper: ricochetLaunchpadHelpder.address,
+  //   },
+  // });
+  // const ricochetLaunchpad = await RicochetLaunchpad.deploy(config.HOST_SUPERFLUID_ADDRESS,
+  //   config.CFA_SUPERFLUID_ADDRESS,
+  //   config.IDA_SUPERFLUID_ADDRESS,
+  //   config.SF_REG_KEY,
+  //   config.REX_REFERRAL_ADDRESS);
+  // await ricochetLaunchpad.deployed();
+  // console.log("Deployed RicochetLaunchpad:", ricochetLaunchpad.address)
 
-  const INPUT_TOKEN_ADDRESS = config.USDCX_ADDRESS;
-  const OUTPUT_TOKEN_ADDRESS = config.RIC_ADDRESS;
+  // const INPUT_TOKEN_ADDRESS = config.USDCX_ADDRESS;
+  // const OUTPUT_TOKEN_ADDRESS = config.RIC_ADDRESS;
 
-  let tx = await ricochetLaunchpad.initialize(
-    INPUT_TOKEN_ADDRESS,
-    OUTPUT_TOKEN_ADDRESS,
-    deployer.address,
-    config.DAO_ADDRESS,
-    EMISSION_RATE,
-    FEE_RATE,
-    { gasLimit: 10000000 });
-  await tx.wait();
-  console.log("Initialized RicochetLaunchpad", tx.hash)
+  // let tx = await ricochetLaunchpad.initialize(
+  //   INPUT_TOKEN_ADDRESS,
+  //   OUTPUT_TOKEN_ADDRESS,
+  //   deployer.address,
+  //   config.DAO_ADDRESS,
+  //   EMISSION_RATE,
+  //   FEE_RATE,
+  //   { gasLimit: 10000000 });
+  // await tx.wait();
+  // console.log("Initialized RicochetLaunchpad", tx.hash)
 
-  // Transfer ownership to the DAO
-  tx = await ricochetLaunchpad.transferOwnership(config.DAO_ADDRESS); 
-  await tx.wait();
-  console.log("Transferred ownership to Gnosis Safe", tx.hash);
-  console.log("Deployed RicochetLaunchpadHelper at address:", ricochetLaunchpadHelpder.address);
-  console.log("Deployed RicochetLaunchpad at address:", ricochetLaunchpad.address);
+  // // Transfer ownership to the DAO
+  // tx = await ricochetLaunchpad.transferOwnership(config.DAO_ADDRESS); 
+  // await tx.wait();
+  // console.log("Transferred ownership to Gnosis Safe", tx.hash);
+  // console.log("Deployed RicochetLaunchpadHelper at address:", ricochetLaunchpadHelpder.address);
+  // console.log("Deployed RicochetLaunchpad at address:", ricochetLaunchpad.address);
 
 
 
